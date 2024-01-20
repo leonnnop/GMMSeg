74f882ed0b01ffbc5ff89bb4829ea5c84b6f9ba3
leonnnop <leonnnop@gmail.com>
2024-01-20 19:21:13 +0800

    upload more configs

---
+++ configs/_gmmseg/segformer_mit-b5_gmmseg_512x512_160k_ade20k.py
@@ -0,0 +1,50 @@
+_base_ = [
+    './models/segformer_mit-b0-gmm.py', '../_base_/datasets/ade20k_aligned.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_160k.py'
+]
+
+model = dict(
+    pretrained='pretrain/mit_b5_mmseg.pth',
+    backbone=dict(
+        embed_dims=64, num_heads=[1, 2, 5, 8], num_layers=[3, 6, 40, 3]),
+    decode_head=dict(
+        channels=768,
+        num_classes=150,
+        in_channels=[64, 128, 320, 512],
+        decoder_params=dict(
+            # * basic gmm setup
+            embed_dim=64,
+            num_components=8,
+            # 
+            factor_n=2,
+            factor_c=1,
+            factor_p=1,
+        ),
+    )
+)
+
+# optimizer
+optimizer = dict(
+    _delete_=True,
+    type='AdamW',
+    lr=0.00006,
+    betas=(0.9, 0.999),
+    weight_decay=0.01,
+    paramwise_cfg=dict(
+        custom_keys={
+            'pos_block': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'head': dict(lr_mult=10.)
+        }))
+
+lr_config = dict(
+    _delete_=True,
+    policy='poly',
+    warmup='linear',
+    warmup_iters=1500,
+    warmup_ratio=1e-6,
+    power=1.0,
+    min_lr=0.0,
+    by_epoch=False)
+
+data = dict(samples_per_gpu=2, workers_per_gpu=2)
